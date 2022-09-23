9fcba1acd8f68a098baa2b0f8073ef0984a23004
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2022-09-23 10:35:33 -0300

    base project

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import BigNumber from "bignumber.js";
+
+class DefiUtils extends BigNumber {
+  WAD_BASIC_UNIT = new BigNumber(1e18);
+
+  constructor(n: BigNumber.Value, base?: number) {
+    super(n, base);
+  }
+
+  fullDecimalsToBigNumber = (decimals: string): DefiUtils => {
+    return new DefiUtils(this.multipliedBy(`1e${decimals}`))
+  }
+
+  bigNumberTofullDecimals = (decimals: string): DefiUtils => {
+    return new DefiUtils(this.dividedBy(`1e${decimals}`))
+  }
+
+  tokensToUnderlyingAmount = (exchangeRate: string): DefiUtils => {
+    return new DefiUtils(new BigNumber(exchangeRate)
+      .multipliedBy(this)
+      .dividedBy(this.WAD_BASIC_UNIT))
+  }
+
+  underlyingAmountToTokens = (exchangeRate: string | BigNumber): DefiUtils => {
+    return new DefiUtils(new BigNumber(this)
+      .multipliedBy(this.WAD_BASIC_UNIT)
+      .multipliedBy(this.WAD_BASIC_UNIT)
+      .dividedBy(exchangeRate)
+      .dividedBy(this.WAD_BASIC_UNIT))
+  }
+
+  valueToUSD = (priceUSD: string | BigNumber): DefiUtils => {
+    return new DefiUtils(new BigNumber(this)
+      .multipliedBy(priceUSD))
+  }
+}
+
+export default DefiUtils;
