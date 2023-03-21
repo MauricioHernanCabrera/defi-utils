8a671b8ef516a4d32eb98c8f0656be23e0dde8a1
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2023-03-21 14:55:46 -0300

    apr to apy fixed

--- src/index.ts
+++ src/index.ts
@@ -8,27 +8,27 @@ class DefiUtils extends BigNumber {
     super(n, base);
   }
 
-  plus = (n: BigNumber.Value, base?: number | undefined) => {
+  override plus = (n: BigNumber.Value, base?: number | undefined) => {
     return new DefiUtils(super.plus(n, base));
   };
 
-  minus = (n: BigNumber.Value, base?: number | undefined) => {
+  override minus = (n: BigNumber.Value, base?: number | undefined) => {
     return new DefiUtils(super.minus(n, base));
   };
 
-  pow = (n: BigNumber.Value, m?: BigNumber.Value) => {
+  override pow = (n: BigNumber.Value, m?: BigNumber.Value) => {
     return new DefiUtils(super.pow(n, m));
   };
 
-  div = (n: BigNumber.Value, base?: number) => {
+  override div = (n: BigNumber.Value, base?: number) => {
     return new DefiUtils(super.div(n, base));
   };
 
-  dividedBy = (n: BigNumber.Value, base?: number) => {
+  override dividedBy = (n: BigNumber.Value, base?: number) => {
     return new DefiUtils(super.dividedBy(n, base));
   };
 
-  multipliedBy = (n: BigNumber.Value, base?: number) => {
+  override multipliedBy = (n: BigNumber.Value, base?: number) => {
     return new DefiUtils(super.multipliedBy(n, base));
   };
 
@@ -60,8 +60,8 @@ class DefiUtils extends BigNumber {
     return new DefiUtils(new BigNumber(this).multipliedBy(priceUSD));
   };
 
-  aprToApy = (apr: BigNumber.Value) => {
-    const calc1 = new BigNumber(apr).dividedBy(365);
+  aprToApy = () => {
+    const calc1 = new BigNumber(this).dividedBy(365);
     const calc2 = new BigNumber(1).plus(calc1);
     const calc3 = new BigNumber(calc2).pow(365);
 
